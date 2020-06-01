3a84cdb1f8dc69aa9c03ac55df6591e6c5a943cc
David Skarbrevik <Skarbrevik_David@bah.com>
2020-05-31 18:04:41 -0700

    ec2 to dynamo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AC-Villager-Ranking
 Animal Crossing Villager Tier Ranking
+
+
+## Pipeline
+
+1) EC2 instance uses a tweepy scraper class to stream twitter data in.
+2) Put each tweet into a DynamoDB table
--- tweepy_listener.py
+++ tweepy_listener.py
@@ -1,8 +1,13 @@
+import boto3
+import json
+
 class ACNHStreamListener(tweepy.StreamListener):
 
-    def __init__(self):
+    def __init__(self, dynamo_table):
         super(ACNHStreamListener, self).__init__()
         self.tweets = []
-
+        self.dynamo = boto3.resource('dynamodb')
+        self.table = self.dynamo.Table(dynamo_table)
     def on_status(self, status):
-        self.tweets.append(status)
+        item = status.__dict__['_json']
+        self.table.put_item(Item=item)
