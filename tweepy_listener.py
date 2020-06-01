aa8c5c88f068d07c549c27dac007f59be366f801
David Skarbrevik <Skarbrevik_David@bah.com>
2020-05-31 18:19:00 -0700

    Update tweepy_listener.py

--- tweepy_listener.py
+++ tweepy_listener.py
@@ -6,7 +6,7 @@ class ACNHStreamListener(tweepy.StreamListener):
     def __init__(self, dynamo_table):
         super(ACNHStreamListener, self).__init__()
         self.tweets = []
-        self.dynamo = boto3.resource('dynamodb')
+        self.dynamo = boto3.resource('dynamodb', region_name='us-east-1')
         self.table = self.dynamo.Table(dynamo_table)
     def on_status(self, status):
         item = status.__dict__['_json']
