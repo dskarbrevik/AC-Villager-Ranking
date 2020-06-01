21a5eb8357357b17311fabbaba9db705c1bb5cda
David Skarbrevik <Skarbrevik_David@bah.com>
2020-05-31 19:54:30 -0700

    Update tweepy_listener.py

--- tweepy_listener.py
+++ tweepy_listener.py
@@ -1,5 +1,7 @@
 import boto3
 import tweepy
+import decimal
+import json
 
 class ACNHStreamListener(tweepy.StreamListener):
 
@@ -8,6 +10,12 @@ class ACNHStreamListener(tweepy.StreamListener):
         self.tweets = []
         self.dynamo = boto3.resource('dynamodb', region_name='us-east-1')
         self.table = self.dynamo.Table(dynamo_table)
+
+    def convert_float(num):
+        return decimal.Decimal(str(round(float(num), 2)))
+
     def on_status(self, status):
         item = status.__dict__['_json']
-        self.table.put_item(Item=item)
+        json_dump = json.dumps(item)
+        json_item = json.loads(json_dump, parse_float=self.convert_float)
+        self.table.put_item(Item=json_item)
