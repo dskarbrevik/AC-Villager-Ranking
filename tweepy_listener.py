193f152d9682b1ed62ab0c41f4b7baa62f7d3b90
David Skarbrevik <Skarbrevik_David@bah.com>
2020-05-31 20:01:10 -0700

    Update tweepy_listener.py

--- tweepy_listener.py
+++ tweepy_listener.py
@@ -15,7 +15,10 @@ class ACNHStreamListener(tweepy.StreamListener):
         return decimal.Decimal(str(round(float(num), 2)))
 
     def on_status(self, status):
-        item = status.__dict__['_json']
-        json_dump = json.dumps(item)
-        json_item = json.loads(json_dump, parse_float=self.convert_float)
-        self.table.put_item(Item=json_item)
+        try:
+            item = status.__dict__['_json']
+            json_dump = json.dumps(item)
+            json_item = json.loads(json_dump, parse_float=self.convert_float)
+            self.table.put_item(Item=json_item)
+        except Exception as e:
+            print(e)
